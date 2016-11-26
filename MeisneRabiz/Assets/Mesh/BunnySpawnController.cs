f8c5fefabbe78dff5add55487ecc631306607b92
Sami Repo <sami.repo@octo3.fi>
2016-11-26 09:47:15 +0200

    Testing features + commits.

---
+++ MeisneRabiz/Assets/Mesh/BunnySpawnController.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BunnySpawnController : MonoBehaviour {
+
+	public GameObject bunnyPrefab; 
+	public int quantity;
+	public int thickness; 
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
