64889976f1b1cd3a40bcecccf0db94f2c2554fac
Sami Repo <sami.repo@octo3.fi>
2016-11-27 08:19:00 +0200

    Heating Reworked.

--- MeisneRabiz/Assets/sami_r/Scripts/HeatSpot.cs
+++ MeisneRabiz/Assets/sami_r/Scripts/HeatSpot.cs
@@ -12,27 +12,23 @@ public class HeatSpot : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		//In here:
-		//1) Check if overlapping / triggered by Bunny... 
-		//2) Heat Up --> Cool Down.. 
-		//3) Trigget animation? 
 
 	}
 
 	public void Cool(int amount) {
-		amountOfHeat = amountOfHeat + amount;
+		amountOfHeat = amountOfHeat - amount;
 		checkUpVolume ();
 	}
 
 	public void Heat(int amount) {
-		amountOfHeat = amountOfHeat - amount;
+		amountOfHeat = amountOfHeat + amount;
 		checkUpVolume ();
 	}
 
 	private void checkUpVolume () {
 		if (amountOfHeat > 10) {
 			//Spot is heating up, launch Hot Spot!
-
+			spawnHeatMarker();
 		}
 
 		if (amountOfHeat < 0) {
@@ -40,10 +36,22 @@ public class HeatSpot : MonoBehaviour {
 
 			//TODO!!! Cool Down // Freeze --> Bunny.
 
+			spawnHeatMarker();
 		}
 	}
 
 	public int getHeat(){
 		return amountOfHeat;
+	}
+
+	void OnTriggerEnter(Collider other) {
+		Debug.Log ("I want TRIGGER");
+		if (other.tag == "bunny") {
+			Heat (3);
+		}
+	}
+
+	void spawnHeatMarker(){
+		GameObject heatMarker = (GameObject)Instantiate (Resources.Load("HeatCollection"));
 	}
 }
--- MeisneRabiz/Assets/sami_r/Scripts/HeatSpotController.cs
+++ MeisneRabiz/Assets/sami_r/Scripts/HeatSpotController.cs
@@ -8,8 +8,7 @@ public class HeatSpotController : MonoBehaviour {
 	private GameObject ground; 
 	public float radiusForHeatSpots;
 
-	public int rows;
-	public int columns;
+	public int size;
 
 	// Use this for initialization
 	void Start () {
@@ -33,21 +32,21 @@ public class HeatSpotController : MonoBehaviour {
 		//Shoot a raycast from center of the cell. 
 		//Summon a new HeatSpot in the center of cell if rayCast hit the ground!. 
 		//Radius = 5???? 
-		for (int y = 0; y < columns - 1; y++) {
-			for (int x = 0; x < rows - 1; x++) {
+		for (int y = 0; y < size - 1; y++) {
+			for (int x = 0; x < size - 1; x++) {
 				//check with raycast! 
 				RaycastHit hit;
-				float posX = -5 + 1 * x;
-				float posY = -5 + 1 * y;
+				float posX = -(size/2) + x;
+				float posY = -(size/2) + y;
 
-				Vector3 position = new Vector3 (posX, 0.5f, posY); 
+				Vector3 position = new Vector3 (posX, 0.01f, posY); 
 
+				GameObject hs = summonHeatSpot (position);
 				Debug.DrawRay (position, transform.TransformDirection (Vector3.down),Color.red,20.0f);
 
 				if (Physics.Raycast(position, transform.TransformDirection(Vector3.down),10.0f)) {
 					Debug.Log ("OmNomNom... Heating. row: " + y);
 
-					GameObject hs = summonHeatSpot (position);
 					hs.GetComponent<HeatSpot> ().amountOfHeat = 1;
 					/*if (hit.collider.gameObject.name == "ground") {
 					}*/
